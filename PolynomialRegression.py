059fcf7d62030887206a2775adc2f0a85014a33d
cardstdani <72665778+cardstdani@users.noreply.github.com>
2021-06-23 16:38:42 +0200

    Update PolynomialRegression.py

--- PolynomialRegression.py
+++ PolynomialRegression.py
@@ -1,20 +1,43 @@
+import random
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler, LabelEncoder
+
+
 
 # Data load
-data = pd.read_csv('My_Data.csv')
-x = data.iloc[:, 1].values
-y = data.iloc[:, 2].values
+data = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv')
+data = data.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
+
+zone = np.where(np.array(data["location"]) == "United States")
+
+x = data.iloc[np.amin(zone) + 1:np.amax(zone) + 1, 2].values
+x = LabelEncoder().fit_transform(x)
+y = data.iloc[np.amin(zone) + 1:np.amax(zone) + 1, 5].values
 
 xTrain = np.array(x).reshape(-1, 1)
 yTrain = np.array(y).reshape(-1, 1)
 
+#Divide data
+trainRate = 0.7
+trainIndex = int(len(xTrain) * trainRate)
+xTest = xTrain[trainIndex:]
+yTest = yTrain[trainIndex:]
+
+xTrain = xTrain[:trainIndex]
+yTrain = yTrain[:trainIndex]
+
+# Scale the data
+# scaler = StandardScaler()
+# xTrain = scaler.fit_transform(xTrain)
+# yTrain = scaler.fit_transform(yTrain)
+
 # Regression
 regression = LinearRegression()
-pFeatures = PolynomialFeatures(degree=1, include_bias=False)
+pFeatures = PolynomialFeatures(degree=2, include_bias=False)
 xPoly = pFeatures.fit_transform(xTrain)
 regression.fit(xPoly, yTrain)
 
@@ -22,19 +45,16 @@ regression.fit(xPoly, yTrain)
 errors = []
 averageError = 0
 
-for i in range(len(xTrain)):
-    error = regression.predict(xPoly)[i] - yTrain[i]
+for i in range(len(xTest)):
+    error = regression.predict(pFeatures.fit_transform(xTest))[i] - yTest[i]
     errors.append(abs(error))
-    plt.plot([xTrain[i], xTrain[i]], [regression.predict(xPoly)[i], regression.predict(xPoly)[i] - error], marker='',
-             color='g')
-averageError = (sum(errors)**2) / len(xTrain)
+    plt.plot(xTest[i], regression.predict(pFeatures.fit_transform(xTest))[i], marker='.', color='g')
+averageError = (sum(errors)) / len(xTest)
 print("Average error: " + str(averageError))
 
 # Plot settings
 plt.plot(x, y, marker='.', color='#3F3BFA', label="Data line")
 plt.plot(xTrain, regression.predict(xPoly), marker='', color='r', label="Regression line")
-xTest = np.array([10]).reshape(-1, 1)
-plt.plot(xTest, regression.predict(pFeatures.fit_transform(xTest)), marker='o', color='y', label="Prediction point")
 plt.grid(color='#1f1f1f', linestyle='dashed', linewidth=0.3)
 plt.gca().set_facecolor('#ffffff')
 plt.legend()

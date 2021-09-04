963d0fa14aa48008e5f342b0ce8319413106a36a
cardstdani <72665778+cardstdani@users.noreply.github.com>
2021-09-04 10:35:18 +0200

    Update PolynomialRegression.py

--- PolynomialRegression.py
+++ PolynomialRegression.py
@@ -5,23 +5,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler, LabelEncoder
-
-
+import sklearn
 
 # Data load
-data = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv')
-data = data.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv")
+df = df.loc[df['location'] == 'World']
 
-zone = np.where(np.array(data["location"]) == "United States")
-
-x = data.iloc[np.amin(zone) + 1:np.amax(zone) + 1, 2].values
-x = LabelEncoder().fit_transform(x)
-y = data.iloc[np.amin(zone) + 1:np.amax(zone) + 1, 5].values
+x = np.array(range(0, len(df['total_vaccinations'])))
+y = np.array(df['total_vaccinations'])
 
 xTrain = np.array(x).reshape(-1, 1)
 yTrain = np.array(y).reshape(-1, 1)
 
-#Divide data
+# Split data
 trainRate = 0.7
 trainIndex = int(len(xTrain) * trainRate)
 xTest = xTrain[trainIndex:]
@@ -30,7 +27,7 @@ yTest = yTrain[trainIndex:]
 xTrain = xTrain[:trainIndex]
 yTrain = yTrain[:trainIndex]
 
-# Scale the data
+# Scale data
 # scaler = StandardScaler()
 # xTrain = scaler.fit_transform(xTrain)
 # yTrain = scaler.fit_transform(yTrain)
@@ -42,21 +39,25 @@ xPoly = pFeatures.fit_transform(xTrain)
 regression.fit(xPoly, yTrain)
 
 # Error calculation
-errors = []
-averageError = 0
+print("Average error: " + str(sklearn.metrics.mean_absolute_error(yTest, regression.predict(pFeatures.fit_transform(xTest)))))
+print("Squared error: " + str(sklearn.metrics.mean_squared_error(yTest, regression.predict(pFeatures.fit_transform(xTest)))))
+"""errors = []
+squaredErrors = []
 
 for i in range(len(xTest)):
     error = regression.predict(pFeatures.fit_transform(xTest))[i] - yTest[i]
     errors.append(abs(error))
-    plt.plot(xTest[i], regression.predict(pFeatures.fit_transform(xTest))[i], marker='.', color='g')
-averageError = (sum(errors)) / len(xTest)
-print("Average error: " + str(averageError))
+    squaredErrors.append(abs(error) ** 2)
+mae = '%f' % (sum(errors) / len(xTest))
+mse = '%f' % (sum(squaredErrors) / len(xTest))
+print("Average error: " + str(mae))
+print("Squared error: " + str(mse))"""
 
 # Plot settings
 plt.plot(x, y, marker='.', color='#3F3BFA', label="Data line")
-plt.plot(xTrain, regression.predict(xPoly), marker='', color='r', label="Regression line")
+plt.plot(xTrain, regression.predict(xPoly), marker='', color='r', label="Train regression line")
+plt.plot(xTest, regression.predict(pFeatures.fit_transform(xTest)), marker='', color='g', label="Test regression line")
 plt.grid(color='#1f1f1f', linestyle='dashed', linewidth=0.3)
 plt.gca().set_facecolor('#ffffff')
 plt.legend()
-# plt.get_current_fig_manager().full_screen_toggle() #Shows the graph in fullscreen
 plt.show()
